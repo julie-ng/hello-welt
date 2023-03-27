df845115fa68e0eca4b00862f69039919ca7d968
Julie Ng <me@julie.io>
2023-03-27 13:47:53 +0200

    feat: add '/slow' to simulate memory leak

--- app/app.js
+++ app/app.js
@@ -49,5 +49,26 @@ module.exports = function (opts ={}) {
     process.exit(1)
   })
 
+  app.get('/slow', async function (req, reply) {
+    function setCallback() {
+      const data = {
+        counter: 0,
+        hugeString: new Array(10000000).join('x')
+      }
+
+      return function cb() {
+        data.counter++ // data object is now part of the callback's scope
+        console.log(data.counter) // intentionally pollute logs
+      }
+    }
+
+    setInterval(setCallback(), 100) // can't stop this 🫠
+
+    reply.send({
+      status: 'pass',
+      message: 'Warning: this path intentionally creates a memory leak! Please use only for testing and demo purposes.'
+    })
+  })
+
   return app
 }
